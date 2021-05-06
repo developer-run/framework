5c451359dbba8281ce67b99b727848d38bce15a9
Pavel Paulík <pavel.paulik@seznam.cz>
2021-05-06 17:05:00 +0200

    fix ImagesExtension.php - Nette 3 configuration

--- Devrun/DI/ImagesExtension.php
+++ Devrun/DI/ImagesExtension.php
@@ -2,7 +2,10 @@
 
 namespace Devrun\DI;
 
+use Devrun\Application\UI\Images\Macros\Latte;
 use Nette;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 
 /**
  * @author Pavel Paulík <pavel.paulik@support.etnetera.cz>
@@ -10,55 +13,54 @@ use Nette;
 class ImagesExtension extends Nette\DI\CompilerExtension
 {
 
-    public $defaults = array(
-        'storageDir' => "%wwwDir%". '/public',
-        'assetsDir' => "%wwwDir%". '/public',
-
-        'data_path'          => '%wwwDir%/../public/data',
-        'data_dir'           => 'data',
-        'algorithm_file'     => 'sha1_file',
-        'algorithm_content'  => 'sha1',
-        'quality'            => 85,
-        'default_transform'  => 'fit',
-        'noimage_identifier' => 'noimage/03/no-image.png',
-        'friendly_url'       => FALSE
-
-    );
-
-
+    public function getConfigSchema(): Schema
+    {
+        return Expect::structure([
+            'publicDir' => Expect::string("%wwwDir%"),
+            'data_path' => Expect::string('%wwwDir%/media'),
+            'data_dir' => Expect::string('media'),
+            'algorithm_file' => Expect::string('sha1_file'),
+            'algorithm_content' => Expect::string('sha1'),
+            'quality' => Expect::int(85),
+            'default_transform' => Expect::string('fit'),
+            'noimage_identifier' => Expect::string('noimage/8f/no-image.png'),
+            'friendly_url' => Expect::bool(false),
+        ]);
+    }
 
 
-	public function loadConfiguration()
-	{
-		$config  = $this->getConfig($this->defaults);
+    public function loadConfiguration()
+    {
+        $config  = $this->getConfig();
 
         /** @var Nette\DI\ContainerBuilder $builder */
         $builder = $this->getContainerBuilder();
 
-		$engine = $builder->getDefinition('nette.latteFactory');
-		$install = 'Devrun\Application\UI\Images\Macros\Latte::install';
+        /** @var Nette\DI\Definitions\FactoryDefinition $engine */
+        $engine = $builder->getDefinition('nette.latteFactory');
 
-		if (method_exists('Latte\Engine', 'getCompiler')) {
-			$engine->addSetup('Devrun\Application\UI\Images\Macros\Latte::install(?->getCompiler())', array('@self'));
-		} else {
-			$engine->addSetup($install . '(?->compiler)', array('@self'));
-		}
+        $install = Latte::class . '::install';
+
+        if (method_exists('Latte\Engine', 'getCompiler')) {
+            $engine->getResultDefinition()->addSetup('Devrun\Application\UI\Images\Macros\Latte::install(?->getCompiler())', array('@self'));
+        } else {
+            $engine->getResultDefinition()->addSetup($install . '(?->compiler)', array('@self'));
+        }
 
         $builder->addDefinition($this->prefix('storage'))
-            ->setFactory('Devrun\Storage\ImageStorage')
-            ->setArguments([
-                $builder->parameters['wwwDir'],
-                $config['data_path'],
-                $config['data_dir'],
-                $config['algorithm_file'],
-                $config['algorithm_content'],
-                $config['quality'],
-                $config['default_transform'],
-                $config['noimage_identifier'],
-                $config['friendly_url']
-            ]);
-
-	}
+                ->setFactory('Devrun\Storage\ImageStorage')
+                ->setArguments([
+                    $config->publicDir,
+                    $config->data_path,
+                    $config->data_dir,
+                    $config->algorithm_file,
+                    $config->algorithm_content,
+                    $config->quality,
+                    $config->default_transform,
+                    $config->noimage_identifier,
+                    $config->friendly_url
+                ]);
+    }
 
 
 }
